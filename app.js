c056fdf852e1930155f2a9b76622fc6715b537a5
annenorenes <anne.norenes@icloud.com>
2026-03-24 15:19:17 +0100

    ruter oppdatering og endring i database

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// Server-bit, setter opp en Express-app
 const express = require('express');
 const app = express();
 
 const PORT = 3000;
 
+// Databasen
 const Database = require('better-sqlite3');
 const db = new Database('musikk.db');
 
+// CORS-middleware for å tillate forespørsler fra andre domener
 const cors = require('cors');
 app.use(cors());
+
+// ...
+app.get('/api/alle_artister', (req, res) => {
+    // ...
+    const artister = db.prepare('SELECT artist_navn FROM artist').all();
+    res.json(artister);
+});
+
+// Åpner en viss port på serveren, og starter serveren
+app.listen(PORT, () => {
+    console.log(`Server kjører på http://localhost:${PORT}`);
+});
