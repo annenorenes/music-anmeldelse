fd2bc75a499b06eae401e7109f8ee4781104fcf6
annenorenes <anne.norenes@icloud.com>
2026-04-07 15:36:59 +0200

    ender tittle, og oppdaterer script

--- public/oversikt_artister/script.js
+++ public/oversikt_artister/script.js
@@ -1,8 +1,7 @@
-// async function henter_alle_artister() {
-//     const response = await fetch('/api/alle_artister');
-//     const data = await response.json();
-// }
+async function henter_alle_artister() {
+    const response = await fetch('/api/alle_artister');
+    const data = await response.json();
+}
 
-// henter_alle_artister();
+henter_alle_artister();
 
-console.log("hei");
