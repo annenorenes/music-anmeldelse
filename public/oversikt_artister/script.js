7ef9a78aad1a7e261c501fe42147d291d86661ef
annenorenes <anne.norenes@icloud.com>
2026-04-07 15:47:30 +0200

    siste oppdatering
    videre så skal jeg opprette en lytte funksjon, slik at den a

--- public/oversikt_artister/script.js
+++ public/oversikt_artister/script.js
@@ -1,6 +1,7 @@
 async function henter_alle_artister() {
     const response = await fetch('/api/alle_artister');
     const data = await response.json();
+    console.log(data);
 }
 
 henter_alle_artister();
