abdda134a789c8616ac393807b25e5e8109538ec
annenorenes <anne.norenes@icloud.com>
2026-04-15 10:24:39 +0200

    oppretter try og catch i js for alle artister

--- public/oversikt_artister/script.js
+++ public/oversikt_artister/script.js
@@ -1,8 +1,20 @@
 // henter ut infroen fra apien, og sender det ut som en array i consolloggen
 async function henter_alle_artister() {
-    const response = await fetch('/api/alle_artister');
-    const data = await response.json();
-    console.log(data);
+    try {
+
+        const response = await fetch('/api/alle_artister');
+        const data = await response.json();
+        console.log(data);
+
+        const container = document.getElementById("artist_liste");
+        container.innerHTML = "";
+
+    }
+    
+    catch (error){
+        console.error("kunne ikke hente artistene:", errror)
+
+    }
 }
 
 henter_alle_artister();
